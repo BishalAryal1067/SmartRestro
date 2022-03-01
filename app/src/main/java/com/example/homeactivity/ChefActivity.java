07103af0eefd6d6b5d35084a0d396034d4cf3b88
Bishal Aryal <bishalaryal1067@gmail.com>
2022-03-01 18:20:38 +0545

    git status

--- app/src/main/java/com/example/homeactivity/ChefActivity.java
+++ app/src/main/java/com/example/homeactivity/ChefActivity.java
@@ -1,14 +1,25 @@
 package com.example.homeactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ChefActivity extends AppCompatActivity {
 
+
+    RecyclerView recycler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef);
+
+       recycler = findViewById(R.id.recycler);
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        recycler.setLayoutManager(layoutManager);
+
     }
 }
--- app/src/main/java/com/example/homeactivity/LoginActivity.java
+++ app/src/main/java/com/example/homeactivity/LoginActivity.java
@@ -1,17 +1,30 @@
 package com.example.homeactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton login_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+       login_button = findViewById(R.id.loginBtn);
+       login_button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent=new Intent(LoginActivity.this, ChefActivity.class);
+               startActivity(intent);
+           }
+       });
     }
 }
 
